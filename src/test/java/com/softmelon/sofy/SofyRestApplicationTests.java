aaadd12992b10b92c79b4984f5b9144205eef151
enochanum <e.anum-trainee@softmelonengineering.com>
2024-07-23 17:37:18 +0000

    added file

---
+++ src/test/java/com/softmelon/sofy/SofyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmelon.sofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
